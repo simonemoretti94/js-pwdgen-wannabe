673800daac785c80ca2a673c78996dce52039dca
simonemoretti94 <simone.moretti.944@gmail.com>
2023-11-15 18:05:43 +0100

    3rd finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,8 @@ console.log(`your name is ${nome_utente}`);
 
 const nome = document.getElementById('td_name');
 
+console.log(`just found ${nome} td1`);
+
 nome.innerText = `${nome_utente}`;
 
 // surname
@@ -17,6 +19,8 @@ let cognome_utente = prompt('Then please, write below your surname');
 console.log(`your surname is ${cognome_utente}`);
 
 const cognome = document.getElementById('td_surname');
+
+console.log(`just found ${cognome} td2`);
 
 cognome.innerText = `${cognome_utente}`;
 
@@ -28,4 +32,16 @@ console.log(`your favourite color is ${user_favcol}`);
 
 const colore = document.getElementById('td_colour');
 
+console.log(`just found ${colore} td3`);
+
 colore.innerText = `${user_favcol}`;
+
+// password generation
+
+let fancyPassword = `${nome_utente}` + `${cognome_utente}` + `${user_favcol}`;
+
+console.log(`fancy password generated is ${fancyPassword}`);
+
+const fancy_password_h1 = document.getElementById('fancy_password');
+
+fancy_password_h1.innerText = `${fancyPassword}`
